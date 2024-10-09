bb69a6530e336f123879bea1c08feb621d3ddcdb
Enot17451 <52169985+Enot17451@users.noreply.github.com>
2024-10-09 23:55:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,67 @@
 from random import *
 
 class Number:
-    
-    def __init(self):
-        self.sign = choice(["+","-","+","-","+","-","+","-","+","-","+","-"])
-        self.number = randint(1,10)
-        
-    def printMinusOnly():
+
+    def __init__(self):
+        self.sign = choice(["+", "-", "+", "-", "+", "-", "+", "-", "+", "-", "+", "-"])
+        self.number = randint(1, 10)
+
+    def __str__(self):
+        return f"{self.number}"
+
+    def printMinusOnly(self):
         if self.sign == "+":
             return f"{self.number}"
         else:
             return f"{self.sign}{self.number}"
-        
+
     def printWithSign(self):
         return f"{self.sign}{self.number}"
-    
+
     def printNoSign(self):
         return f"{self.number}"
-        
+
+
 class Sign:
-    
-    def __init(self):
-        self.sign = choice(["+","-","+","-","+","-","+","-","+","-","+","-"])
-        
-    def __str__(self):
-        return f"se"
-        
-class Var:
-    
+
     def __init__(self):
-        self.number = randint(1,10)
+        self.sign = choice(["+", "-", "+", "-", "+", "-", "+", "-", "+", "-", "+", "-"])
+
+    def __str__(self):
+        return f"{self.sign}"
+
+
+class Var:
+
+    def __init__(self):
+        self.number = randint(1, 10)
         self.letter = "x"
-        self.sign = choice(["+","-","+","-","+","-","+","-","+","-","+","-"])        
-    
+        self.sign = choice(["+", "-", "+", "-", "+", "-", "+", "-", "+", "-", "+", "-"])
+
+    def __str__(self):
+        temp = ""
+        if self.sign != "+":
+            temp += "-"
+        if self.number != 1:
+            temp += str(self.number)
+        return f"{temp}{self.letter}"
+
 
 class Function:
-    
-    def __init__(self):
+
+    def __init__(self,countX):
         self.x = Var()
         self.sign = Sign()
         self.b = Number()
         self.y = 0
-        
-    def __str__(self):
-        return f"y = {self.x} {self.sign} {self.b}"
+        self.xs = []
+        for i in range(countX):
+            self.xs.append(randint(-10, 10))
 
-n = 10
+    def __str__(self):
+        return f"y = {self.x} {self.sign} {self.b} \n   x = {self.xs}"
+
+
+n = 20
 for x in range(n):
-    print(Function())
+    print(Function(3))
